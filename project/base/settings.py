f24bc5f967df97c55eeca57fe578a4edc991d838
Tiffany Hakseth <tjhakseth@gmail.com>
2016-05-11 23:06:51 -0700

    Updated Django structure

--- project/base/settings.py
+++ project/base/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'checks.apps.ChecksConfig',
     'accounts',
+    'base',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
